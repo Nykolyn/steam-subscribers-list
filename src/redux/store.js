b92908d42359cead7ae884dd7f23f6c4fe977662
Andrii Nykolyn <nykolynandrii@gmail.com>
2019-12-22 02:34:29 +0200

    prod ver

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,12 @@ import authReducer from './reducers/authReducer';
 const rootReducer = combineReducers({ isAuth: authReducer });
 
 const middleware = [];
-
+/* eslint-disable-next-line */
 const enhancer =
   process.env.NODE_ENV === 'development'
     ? composeWithDevTools(applyMiddleware(...middleware))
     : applyMiddleware(...middleware);
 
-const store = createStore(rootReducer, enhancer);
+const store = createStore(rootReducer);
 
 export default store;
