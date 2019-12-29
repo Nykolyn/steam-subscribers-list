5d0e86c6a53b57275dce29d03d640d92e5e491c4
Andrii Nykolyn <nykolynandrii@gmail.com>
2019-12-29 14:24:06 +0200

    added service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as serviceWorker from './serviceWorker';
 import Root from './Components/Root/Root';
 import './scss/main.scss';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
 console.log(process.env.NODE_ENV);
+serviceWorker.register();
